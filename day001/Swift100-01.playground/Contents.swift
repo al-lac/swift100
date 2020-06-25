d65c9db31b7b87cf96f917337feea539fbcf1c35
Alexander Lackner <lackner.alex@gmail.com>
2020-06-26 00:23:40 +0200

    Day 01

---
+++ day001/Swift100-01.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+var str = "Hello, playground"
+
+str = "Test"
+
+var population = 8_000_000
+
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+var pi = 3.141
+
+var awesome = true
+
+var score = 85
+var strtest = "Your score was \(score)"
+
+let taylor = "swift"
+
+let str2 = "Hello, playground"
+
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
